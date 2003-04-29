ad1cf4cbb6d032b3155a66b33ecc9184aee3f8df
namesys.com!umka <namesys.com!umka>
2003-04-29 10:57:40 +0000

    (Logical change 1.5)

---
+++ src/libaal.c
@@ -0,0 +1,26 @@
+/*
+  libaal.c -- library whide functions.
+    
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  libaal/COPYING.
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+/* Returns libaal max supported interface version */
+int libreiser4_max_interface_version(void) {
+	return LIBAAL_MAX_INTERFACE_VERSION;
+}
+
+/* Returns libaal min supported interface version */
+int libreiser4_min_interface_version(void) {
+	return LIBAAL_MIN_INTERFACE_VERSION;
+}
+
+/* Returns libaal version */
+const char *libaal_version(void) {
+	return VERSION;
+}
+
