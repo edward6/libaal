0819845e5fa7b1b92facaee54a2d1b4e0ad55dd7
namesys.com!umka <namesys.com!umka>
2003-06-08 14:32:06 +0000

    Added hash_table. Not finished yet.
    2003/06/08 15:31:22+04:00 namesys.com!umka
    
    (Logical change 1.49)

---
+++ include/aal/hash.h
@@ -0,0 +1,29 @@
+/*
+  hash.h -- hash table implementation.
+    
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  libaal/COPYING.
+*/
+
+#ifndef AAL_HASH_H
+#define AAL_HASH_H
+
+#include <aal/types.h>
+
+extern void aal_hash_table_free(aal_hash_table_t *table);
+
+extern int aal_hash_table_remove(aal_hash_table_t *table,
+				 void *key);
+
+extern void aal_hash_table_insert(aal_hash_table_t *table,
+				  void *key, void *value);
+
+extern void *aal_hash_table_lookup(aal_hash_table_t *table,
+				   void *key);
+
+extern aal_hash_table_t *aal_hash_table_alloc(hash_func_t hash_func,
+					      comp_func_t comp_func);
+
+extern aal_hash_node_t **aal_hash_table_lookup_node(aal_hash_table_t *table,
+						    void *key);
+#endif
